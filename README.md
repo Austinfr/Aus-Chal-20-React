656e81e5478ed89a96a100eba334c570ed881579
Austie <wustinah20@gmail.com>
2023-05-03 20:38:04 -0700

    update App.js

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Aus-Chal-20-React
+# Austin Week 20 Challenge React Portfolio, Reactfolio
 
-Does this update the website?
+## Description
+
+Showing off my portfolio of the work and projects that I have done and that I can create a react app to house all of this
+
+## TOC/Links
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Testing](#testing)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+delete this
+
+## Usage
+
+Visit the website at (put link here) and play around with the ui
+
+## Contribution
+
+Send any comments or feedback my way at either of [my links](#questions)
+
+## Testing
+
+see what links work and what you can submit
+
+## License
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+
+## Questions
+
+If there are any questions you can contact me at wustinah20@gmail.com or refer to https://github.com/austinfr 
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
@@ -14,11 +14,13 @@ function App() {
     <Router>
         <Header />
         <Navigation />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/resume" component={Resume} />
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/portfolio" element={<Portfolio/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/resume" element={<Resume/>} />
+        </Routes>
         <Footer />
     </Router>
   );
